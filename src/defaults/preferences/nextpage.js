e436ecd9b134b698475966effb008f51b6ae4a19
Yuanle Song <sylecn@gmail.com>
2009-12-07 04:13:39 -0600

    removed all prefwindow; remove skin/overlay.css

--- src/defaults/preferences/nextpage.js
+++
@@ -1,3 +0,0 @@
-pref("extensions.nextpage.boolpref", false);
-// See http://kb.mozillazine.org/Localize_extension_descriptions
-pref("extensions.nextpage@yuanle.song.description", "chrome://nextpage/locale/nextpage.properties");
